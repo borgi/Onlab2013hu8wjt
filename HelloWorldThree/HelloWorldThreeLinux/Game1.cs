b5e25c561a3918c6fc657eb94402d7d25ba7b34b
borgi <borgihun@gmail.com>
2013-03-31 19:12:15 +0200

    a

---
+++ HelloWorldThree/HelloWorldThreeLinux/Game1.cs
@@ -0,0 +1,92 @@
+#region Using Statements
+using System;
+
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Storage;
+using Microsoft.Xna.Framework.Input;
+
+#endregion
+
+namespace HelloWorldThreeLinux
+{
+	/// <summary>
+	/// This is the main type for your game
+	/// </summary>
+	public class Game1 : Game
+	{
+		GraphicsDeviceManager graphics;
+		SpriteBatch spriteBatch;
+		SpriteFont sf;
+		Vector2 textpos = new Vector2(0,0);
+
+		public Game1 ()
+		{
+			graphics = new GraphicsDeviceManager (this);
+			Content.RootDirectory = "Content";	            
+			//graphics.IsFullScreen = true;		
+		}
+
+		/// <summary>
+		/// Allows the game to perform any initialization it needs to before starting to run.
+		/// This is where it can query for any required services and load any non-graphic
+		/// related content.  Calling base.Initialize will enumerate through any components
+		/// and initialize them as well.
+		/// </summary>
+		protected override void Initialize ()
+		{
+			// TODO: Add your initialization logic here
+			base.Initialize ();
+				
+		}
+
+		/// <summary>
+		/// LoadContent will be called once per game and is the place to load
+		/// all of your content.
+		/// </summary>
+		protected override void LoadContent ()
+		{
+			// Create a new SpriteBatch, which can be used to draw textures.
+			spriteBatch = new SpriteBatch (GraphicsDevice);
+			sf = Content.Load<SpriteFont>("SpriteFont1");
+
+			//TODO: use this.Content to load your game content here 
+		}
+
+		/// <summary>
+		/// Allows the game to run logic such as updating the world,
+		/// checking for collisions, gathering input, and playing audio.
+		/// </summary>
+		/// <param name="gameTime">Provides a snapshot of timing values.</param>
+		protected override void Update (GameTime gameTime)
+		{
+			// For Mobile devices, this logic will close the Game when the Back button is pressed
+			if (GamePad.GetState (PlayerIndex.One).Buttons.Back == ButtonState.Pressed) {
+				Exit ();
+			}
+			// TODO: Add your update logic here			
+			base.Update (gameTime);
+
+			textpos.X = (textpos.X + 0.05F) % 100;
+		}
+
+		/// <summary>
+		/// This is called when the game should draw itself.
+		/// </summary>
+		/// <param name="gameTime">Provides a snapshot of timing values.</param>
+		protected override void Draw (GameTime gameTime)
+		{
+			graphics.GraphicsDevice.Clear (Color.CornflowerBlue);
+		
+			//TODO: Add your drawing code here
+            
+			spriteBatch.Begin();
+			//spriteBatch.Draw(image, Vector2.Zero, Color.White);
+			spriteBatch.DrawString(sf, "Hello World!", textpos, Color.White);
+			spriteBatch.End();
+
+			base.Draw (gameTime);
+		}
+	}
+}
+
---
+++ MonoGameHelloWorld/HelloWorldOne/HW1/LocalizedStrings.cs
@@ -0,0 +1,14 @@
+﻿using HW1.Resources;
+
+namespace HW1
+{
+    /// <summary>
+    /// Provides access to string resources.
+    /// </summary>
+    public class LocalizedStrings
+    {
+        private static AppResources _localizedResources = new AppResources();
+
+        public AppResources LocalizedResources { get { return _localizedResources; } }
+    }
+}
